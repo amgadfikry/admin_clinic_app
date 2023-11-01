13db14bd8e9d120f603e041a6999a35a9e7ebf01
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-01 08:21:49 +0000

    update packages

--- back-end/packages.sh
+++ back-end/packages.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 sudo apt update
+sudo apt-get install python3-pip
 sudo pip install --upgrade pip
 sudo apt-get install pkg-config
 sudo apt-get install mysql-server
-sudo pip install Werkzeug
+sudo apt install python3-flask
+sudo pip3 install Werkzeug
 sudo pip install Flask Flask-CORS Flask-JWT-Extended pyjwt
 sudo apt-get install libmysqlclient-dev
 sudo apt-get install python3-dev
 sudo apt-get install zlib1g-dev
-sudo pip install mysqlclient
-sudo pip install mysqlclient
-sudo pip install SQLAlchemy
+sudo pip3 install mysqlclient
+sudo pip3 install SQLAlchemy
