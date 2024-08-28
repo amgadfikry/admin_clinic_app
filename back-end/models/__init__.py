ad97ee3d616c32019bca8b06e831801bee0b35f5
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-08-28 16:44:29 +0300

    add logic that create admin account that have access to dashboard when server is start and connect to database

--- back-end/models/__init__.py
+++ back-end/models/__init__.py
@@ -15,10 +15,11 @@ from models.time import Time
 from models.offer import Offer
 from models.testimonial import Testimonial
 from models.admin import Admin
+from werkzeug.security import generate_password_hash
 
 
 # create engine with mysql database, user is api, host is local, and clinic_db database
-engine = create_engine('mysql+mysqldb://api@localhost/clinic_db')
+engine = create_engine('mysql+mysqldb://api_test:test@mysql:3306/clinic_test_db')
 
 # create all table by add it to metadata of Base class of sqlalchemy
 Base.metadata.create_all(engine)
@@ -26,3 +27,28 @@ Base.metadata.create_all(engine)
 # create session and initiate it
 session_creator = sessionmaker(bind=engine, expire_on_commit=False)
 Session = scoped_session(session_creator)
+
+# function make admin if not exists in database
+def create_admin_if_not_exists():
+	try:
+		admin_exist = Session.query(Admin).filter_by(admin_name="admin").first()
+		if not admin_exist:
+			admin_data = {
+			'admin_name': 'admin',
+			'email': 'admin@example.com',
+			'password': generate_password_hash('admin', method='scrypt'),
+			'user_name': 'admin'
+			}
+			new_admin = Admin(**admin_data)
+			Session.add(new_admin)
+			Session.commit()
+			print("Admin created successfully.")
+		else:
+			print("Admin already exists.")
+	except Exception as e:
+		Session.rollback()
+		print(f"An error occurred: {e}")
+	finally:
+		Session.remove()
+
+create_admin_if_not_exists()
