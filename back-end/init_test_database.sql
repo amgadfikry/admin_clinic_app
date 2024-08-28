873028fd2bfabf91f1dbf1cd5e7d9fb249b0e228
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-08-28 16:43:37 +0300

    change sql scipt that create user and database in test mode to be available on any host otherthan localhost and to have password

--- back-end/init_test_database.sql
+++ back-end/init_test_database.sql
@@ -1,11 +1,11 @@
-# Create a new test database if it does not exist #
+-- Create a new test database if it does not exist
 CREATE DATABASE IF NOT EXISTS clinic_test_db;
 
-# Create a user 'api_test' if it does not exist #
-CREATE USER IF NOT EXISTS 'api_test'@'localhost';
+-- Create a user 'api_test' with a password if it does not exist
+CREATE USER IF NOT EXISTS 'api_test'@'%' IDENTIFIED BY 'test';
 
-# Grant all privileges on the 'clinic_test_db' database to the 'apiflask' user #
-GRANT ALL PRIVILEGES ON clinic_test_db.* TO 'api_test'@'localhost';
+-- Grant all privileges on the 'clinic_test_db' database to the 'api_test' user
+GRANT ALL PRIVILEGES ON clinic_test_db.* TO 'api_test'@'%';
 
-# Flush privileges to apply the changes immediately #
+-- Flush privileges to apply the changes immediately
 FLUSH PRIVILEGES;
